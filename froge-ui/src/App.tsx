3509457e73d8d7dfb25f5f8f250d2e5671322bfb
Semi Invader <=>
2023-05-01 11:42:31 -0600

    added background and highlights

--- froge-ui/src/App.tsx
+++ froge-ui/src/App.tsx
@@ -1,11 +1,17 @@
+import { useRef } from "react";
 import frogeLogo from "/logo.png";
-import { AiFillTwitterCircle, AiFillMediumCircle } from "react-icons/ai";
+import {
+  AiFillTwitterCircle,
+  AiFillMediumCircle,
+  AiFillGithub,
+} from "react-icons/ai";
 import { BsTelegram } from "react-icons/bs";
 
 function App() {
+  const footerRef = useRef<HTMLDivElement>(null);
   return (
     <main className="flex flex-col items-center min-h-screen">
-      <header className="flex flex-row justify-center w-full py-6 bg-primary">
+      <header className="flex flex-row justify-center w-full py-6 bg-primary px-4">
         <div className="flex flex-row justify-between items-center container">
           <div className="flex flex-row items-center">
             <div className="relative rounded-full w-20 h-20 overflow-hidden border-2 border-white">
@@ -19,7 +25,15 @@ function App() {
             <div className="text-white text-4xl pl-6">Froge</div>
           </div>
           <nav className="text-xl flex flex-row gap-x-6">
-            <a>Socials</a>
+            <button
+              className="text-black bg-white p-2 rounded-xl hover:text-white hover:bg-black"
+              aria-label="Scroll to socials"
+              onClick={() =>
+                footerRef.current?.scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              Socials
+            </button>
           </nav>
         </div>
       </header>
@@ -38,18 +52,10 @@ function App() {
         <div className="text-center text-3xl text-red-500">
           ZERO FUCKING TAXES
         </div>
-        <div className="flex flex-row justify-center gap-x-4 pt-12 text-center text-xl">
-          It's so early, we're still building our landing page, but here's a{" "}
-        </div>
-        <div className="pb-12">
-          <a
-            href="https://medium.com/@frogethegod/froge-the-god-pure-meme-perfection-meh-why-not-5b1afca6da8f"
-            target="_blank"
-            rel="noopener"
-            className="text-blue-300 underline hover:text-blue-500 text-2xl "
-          >
-            MEDIUM article
-          </a>
+        <div className="flex flex-row justify-center gap-x-4 pt-4 text-center text-xl font-sans">
+          Froge the God is here. Do you need it? Probably not. But you want it
+          because you know you love this shit! You like Dog and Frog shitcoins,
+          well here&apos;s the God of them all!
         </div>
         <div>Contract Address</div>
         <a
@@ -79,7 +85,33 @@ function App() {
           </a>
         </nav>
       </div>
-      <footer className="flex flex-row justify-center bg-primary w-full py-6">
+      <section className="mario-bg w-full min-h-36 py-12 flex flex-col items-center">
+        <h2 className="text-center text-black text-6xl pb-6">Highlights</h2>
+        <div className="grid grid-flow-row container grid-cols-2 font-sans font-bold px-4 gap-x-4 gap-y-6">
+          <ul className="highlight">No Pre-sale</ul>
+          <ul className="highlight">No Tax</ul>
+          <ul className="highlight">No Team Tokens</ul>
+          <ul className="highlight">Burned Liquidity</ul>
+          <ul className="highlight">Renounced Token</ul>
+          <ul className="highlight col-span-2 md:col-span-1">
+            90% of tokens added to Liquidity
+          </ul>
+          <ul className="highlight col-span-2 md:col-span-1">
+            12.5% of the supply already bought and burned
+          </ul>
+          <ul className="highlight col-span-2 md:col-span-1">
+            Audit (in progress, link will be posted here when final)
+          </ul>
+          <ul className="highlight col-span-2 md:col-span-1">
+            Road map includes creating more safety for investors and projects,
+            building with other like-minded projects, and much more
+          </ul>
+        </div>
+      </section>
+      <footer
+        className="flex flex-col items-center bg-primary w-full py-6"
+        ref={footerRef}
+      >
         <nav className="flex flex-row justify-center gap-x-4">
           <a
             className="rounded-full overflow-hidden hover:text-blue-500 hover:bg-white"
@@ -105,7 +137,16 @@ function App() {
           >
             <AiFillMediumCircle className="text-4xl " />
           </a>
+          <a
+            className="rounded-full overflow-hidden hover:text-black hover:bg-white"
+            href="https://github.com/Froge-WTF/froge"
+            target="_blank"
+            rel="noopenner"
+          >
+            <AiFillGithub className="text-4xl " />
+          </a>
         </nav>
+        <p className="font-sans pt-5 text-sm">Copyright Froge The God 2023©️</p>
       </footer>
     </main>
   );

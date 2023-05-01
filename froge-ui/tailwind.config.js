cd602ea52c019d473d04898fb7fa2c94bd8a398f
Semi Invader <=>
2023-05-01 10:47:01 -0600

    added primary color

--- froge-ui/tailwind.config.js
+++ froge-ui/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#449F3C"
+      }
+    },
   },
   plugins: [],
 }
